4564af556d6aeffa4f39dec704389cc1b8f830f8
Hryhorii Khyzhniakov <soults15@gmail.com>
2021-06-03 20:40:07 +0300

    REST API call in getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,22 @@
 import Layout from '../components/Layout';
 
-const Index = () => <Layout>main</Layout>;
+const Index = ({ countries }) => (
+    <Layout>
+        <div className='count'>
+            Found {countries.length} countries
+        </div>
+    </Layout>
+);
+
+export const getStaticProps = async () => {
+    const res       = await fetch('https://restcountries.eu/rest/v2/all');
+    const countries = await res.json();
+
+    return {
+        props: {
+            countries,
+        },
+    };
+};
 
 export default Index;
